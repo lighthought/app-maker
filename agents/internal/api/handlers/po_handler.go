05a8852d83dfda52408f1f741407933d319cd2fc
zhujie2006 <zhujie.2006@163.com>
2025-10-08 14:13:48 +0800

    【fix】切换 kimi 后，PO 生成的用户故事不带序号。在提示词中强制增加序号，以供后续维护。

--- agents/internal/api/handlers/po_handler.go
+++ agents/internal/api/handlers/po_handler.go
@@ -38,8 +38,9 @@ func (s *PoHandler) GetEpicsAndStories(c *gin.Context) {
 
 	message := "@bmad/po.mdc 我希望你基于PRD文档 @" + req.PrdPath + " 和 @" + req.ArchFolder +
 		" 目录下的架构设计，创建 Epics（史诗）和 Stories（用户故事）。\n" +
-		"生成不分片的 Epics，输出到 docs/ 目录下单个文件 epics.md。再根据 Epics 生成分片的用户故事，按 epics 的名称输出到 docs/stories/ 目录下多个文件。" +
-		"注意：stories 中要包含验收标准。不要考虑安全、合规。"
+		"生成 Epics，输出到 docs/ 目录下，文件名为： epics-stories.md。\n" +
+		"注意：1. 输出分片的 Stories，输出到 docs/stories/ 目录下。文件名用 epic 的名称命名，后缀用 -story.md。\n" +
+		"2. stories 中要包含验收标准。不要考虑安全、合规。每个用户故事都要有自己的编号，方便后续记录、跟踪。"
 
 	taskInfo, err := s.agentTaskService.Enqueue(req.ProjectGuid, common.AgentTypePO, message)
 	if err != nil {
